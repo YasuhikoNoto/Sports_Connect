eaf41e95946cd8ac66af4aad8d1d3c7be98d09db
EC2 Default User <ec2-user@ip-172-31-1-76.ap-northeast-1.compute.internal>
2023-08-29 10:35:18 +0000

    [finish]v1.0

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,24 +1,24 @@
 class ApplicationController < ActionController::Base
-before_action :search
 #before_action :authenticate_admin!, if: :admin_url  adminでadminのアクセス制限をかけたいが、原因不明のエラーが発生
 #暫定措置としてadminの各コントローラにて制限をかける
+# before_action :search
 
-  def search
-    @q = Post.ransack(params[:q])
-    @posts_search = @q.result(distinct: true)
-    @result = params[:q]&.values&.reject(&:blank?)
+#   def search
+#     @q = Post.ransack(params[:q])
+#     @posts_search = @q.result(distinct: true)
+#     @result = params[:q]&.values&.reject(&:blank?)
 
-    #タグ検索
-    @tag_ids = params[:tag_ids]&.select(&:present?)
-    if @tag_ids.present?
-      @tag_word = "タグ: "
-      @tag_ids.each do |id|
-        @tag_word = @tag_word + ' ' + Tag.find(id).item if id != ""
-      end
-    @posts = @posts.joins(:post_tags).where(post_tags: {tag_id: @tag_ids}).group("posts.id").having("count(*) = #{@tag_ids.length}")
-    end
+#     #タグ検索
+#     @tag_ids = params[:tag_ids]&.select(&:present?)
+#     if @tag_ids.present?
+#       @tag_word = "タグ: "
+#       @tag_ids.each do |id|
+#         @tag_word = @tag_word + ' ' + Tag.find(id).item if id != ""
+#       end
+#     @posts = @posts.joins(:post_tags).where(post_tags: {tag_id: @tag_ids}).group("posts.id").having("count(*) = #{@tag_ids.length}")
+#     end
 
-    # 検索結果件数
-    @posts_count = @posts
-  end
+#     # 検索結果件数
+#     @posts_count = @posts
+#   end
 end
--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -2,8 +2,8 @@ class Public::HomesController < ApplicationController
   def top
     @posts = Post.all.order(updated_at: :desc,reated_at: :desc).page(params[:page]).per(5)
     @q = Post.ransack(params[:q])
-    @posts_search = @q.result(distinct: true)
-    @result = params[:q]&.values&.reject(&:blank?)
+    # @posts_search = @q.result(distinct: true)
+    # @result = params[:q]&.values&.reject(&:blank?)
   end
 
   def search
